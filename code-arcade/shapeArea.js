a3efe4850e0b88ce152457cd28eec7a448b24048
Minji Cho <mturtle051@gmail.com>
2022-06-22 14:59:18 -0700

    Update shapeArea.js

--- code-arcade/shapeArea.js
+++ code-arcade/shapeArea.js
@@ -1,6 +1,7 @@
 // Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
 
-// A 1 - interesting polygon is just a square with a side of length 1. An n - interesting polygon is obtained by taking the n - 1 - interesting polygon and appending 1 - interesting polygons to its rim, side by side.
+// A 1 - interesting polygon is just a square with a side of length 1. 
+//An n - interesting polygon is obtained by taking the n - 1 - interesting polygon and appending 1 - interesting polygons to its rim, side by side.
 
 function shapeArea(n) {
   var area = Math.pow(n, 2) + Math.pow(n - 1, 2);
