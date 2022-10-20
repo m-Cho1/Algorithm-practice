9f5711c9b3b4c0528243e50541a22b557a7e4c15
m-Cho1 <mturtle051@gmail.com>
2022-10-20 19:46:56 +0000

    completed coupon check

---
+++ codewars/couponCheck.js
@@ -0,0 +1,24 @@
+/**
+ * Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+
+A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+
+Examples:
+checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+ */
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+  const currDate = new Date(currentDate);
+  const expDate = new Date(expirationDate);
+
+  if (enteredCode === correctCode && currDate < expDate) {
+    return true;
+  } else if (enteredCode === correctCode && +currDate === +expDate) {
+    return true;
+  } else if (enteredCode !== correctCode || currDate > expDate) {
+    return false;
+  }
+}
+
+console.log(`checkCoupon("123", "123", "July 9, 2015", "July 2, 2015") will return false | ${checkCoupon('123', '123', 'July 9, 2015', 'July 2, 2015')}`);
